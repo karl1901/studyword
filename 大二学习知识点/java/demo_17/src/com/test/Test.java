0b48e8f439a462a9a7aa4152458592bd8f56e8ba
karl <2371265527@qq.com>
2021-04-02 08:08:36 +0800

    java-demo-17乱码问题修正

--- 大二学习知识点/java/demo_17/src/com/test/Test.java
+++ 大二学习知识点/java/demo_17/src/com/test/Test.java
@@ -7,40 +7,33 @@ import java.util.Scanner;
 
 /**
  * @author 23712
- *
+ * 
  */
 public class Test {
 
 	/**
 	 * @param args
-	 * 
-	 * �쳣(����)
-	 * 1���������Υ�������Ե��﷨��
-	 * 2�����д���
-	 * Exception�����д������͵ĸ��࣬�ܲ������еı���
-	 * 
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		int[] s=new int[3];
-		Scanner mys=new Scanner(System.in);
-		System.out.println("��������ӣ�");
-		try {  // �ſ��ܳ������д���Ĵ���
-			int x=mys.nextInt();
-			System.out.println("�������ĸ��");
-			int y=mys.nextInt();
-			System.out.println("������һ��������");
-			int z=mys.nextInt();
-			System.out.println(x/y);
+
+		int[] s = new int[3];
+		Scanner mys = new Scanner(System.in);
+		System.out.println("sys01");
+		try {
+			int x = mys.nextInt();
+			System.out.println("sys02");
+			int y = mys.nextInt();
+			System.out.println("sys03");
+			int z = mys.nextInt();
+			System.out.println(x / y);
 			System.out.println(s[z]);
-		} catch (Exception e) {  // ��׽����
+		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
-		}finally {  // �Ų��ܴ����Ƿ�����������ֻҪ��������Ĵ��붼�ᱻ���� 
-			
-		}
+		} finally {
 
+		}
 
 	}
 
