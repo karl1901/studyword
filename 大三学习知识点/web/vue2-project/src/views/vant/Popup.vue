c362eb37297224a5257b65d33dbffba4496c3a4f
karl <2371265527@qq.com>
2021-11-03 16:41:01 +0800

    学习知识点提交==>11/03/2021 16:41:01

--- 大三学习知识点/web/vue2-project/src/views/vant/Popup.vue
+++ 大三学习知识点/web/vue2-project/src/views/vant/Popup.vue
@@ -8,6 +8,13 @@
       <van-button type="info" @click="showToast">toast</van-button>
       &nbsp;
       <van-button type="warning" @click="showPopup('karl')">popup</van-button>
+      &nbsp;
+      <van-button type="danger" @click="showConfirm">确认对话框</van-button>
+    </div>
+
+    <!-- 按钮 -->
+    <div class="pd10">
+      <van-button type="info" @click="showCustom">组件调用对话框</van-button>
     </div>
 
     <!-- 弹出框内容提交表单 -->
@@ -40,6 +47,18 @@
         </div>
       </van-popup>
     </div>
+
+    <!-- dialog-组件调用模式 -->
+    <div>
+      <van-dialog
+        v-model="visiable"
+        title="自定义标题"
+        :beforeClose="closeCustom"
+        :show-cancel-button="true"
+      >
+        <div class="dia">自定义对话框的内容..</div>
+      </van-dialog>
+    </div>
   </div>
 </template>
 
@@ -52,10 +71,37 @@ export default {
       show: false,
       showInfo: '',
       dialogTitle: '',
-      dialogInfo: ''
+      dialogInfo: '',
+      visiable: false
     };
   },
   methods: {
+    closeCustom(action, done) {
+      console.log(action, done);
+      if ('cancel' == action) {
+        done();
+      } else {
+        setTimeout(done, 2000);
+      }
+    },
+    showCustom() {
+      this.visiable = true;
+    },
+    showConfirm() {
+      this.$dialog
+        .confirm({
+          title: '确认对话框',
+          message: '内容....',
+          confirmButtonText: '保存',
+          cancelButtonText: '关闭'
+        })
+        .then(() => {
+          console.log('点击了保存');
+        })
+        .catch(() => {
+          console.log('点击了关闭');
+        });
+    },
     showDialog() {
       this.$dialog.alert({
         message: this.dialogInfo,
